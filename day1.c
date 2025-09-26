81b39459dcd7b988625ff422b7f718c9fec3f611
Sahil Chandel <Sahil.21895@stu.upes.ac.in>
2025-09-26 10:00:00 +0530

    Day 1 - Hello World in C

---
+++ day1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Day 1: Hello World!\n");
+    return 0;
+}
---
+++ day2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/// @brief 
+/// @return 
+int main() {
+    printf("Day 1: Hello World!\n");
+    return 0;
+}
